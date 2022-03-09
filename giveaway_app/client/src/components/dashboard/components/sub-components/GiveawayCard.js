a69f47d28c118bd6b60232dd58c33840335d9c31
Bashar Othman <othman90@gmail.com>
2022-03-09 16:10:21 +0300

    SocialIconLinks

--- giveaway_app/client/src/components/dashboard/components/sub-components/GiveawayCard.js
+++ giveaway_app/client/src/components/dashboard/components/sub-components/GiveawayCard.js
@@ -1,11 +1,11 @@
 import { HStack, VStack, Heading, Tag, Text } from '@chakra-ui/react'
 import React from 'react'
+import { SocialIconLink } from '../../../helpers/SocialIconLink'
 import GiveawayImage from './GiveawayImage'
 
 const GiveawayCard = ({ giveaway }) => {
 	return (
 		<HStack border={'1px'} borderRadius={'10px'} w={'full'} p={'5px'}>
-			{console.log(giveaway)}
 			{giveaway.giveaway_images ? (
 				<GiveawayImage photo={giveaway.giveaway_images[0]} />
 			) : (
@@ -14,7 +14,7 @@ const GiveawayCard = ({ giveaway }) => {
 
 			<VStack>
 				<Text>{giveaway.name}</Text>
-				<Tag>{giveaway.category}</Tag>
+				<SocialIconLink url={giveaway.giveaway_link} />
 			</VStack>
 		</HStack>
 	)
---
+++ giveaway_app/client/src/components/helpers/SocialIconLink.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react'
+import { FaLink } from '@react-icons/all-files/fa/FaLink'
+import { FaFacebookF } from '@react-icons/all-files/fa/FaFacebookF'
+import { SiInstagram } from '@react-icons/all-files/si/SiInstagram'
+import { FiTwitter } from '@react-icons/all-files/fi/FiTwitter'
+import { FiTwitch } from '@react-icons/all-files/fi/FiTwitch'
+import { FiYoutube } from '@react-icons/all-files/fi/FiYoutube'
+import { AspectRatio, Link, Text } from '@chakra-ui/react'
+
+export const SocialIconLink = ({ url }) => {
+	const [icon, setIcon] = useState(<FaLink />)
+	const [socialName, setSocialName] = useState('')
+
+	// const getIcon = () => {
+	// 	const socialsNames = [
+	// 		'facebook',
+	// 		'instagram',
+	// 		'twitter',
+	// 		'amazon',
+	// 		'youtube',
+	// 	]
+	// 	socialsNames.forEach((name) => {
+	// 		if (url.includes(name)) {
+	// 			console.log(url)
+	// 			setIcon(<FaLink />)
+	// 			// setIcon(name)
+	// 		}
+	// 	})
+	// }
+
+	useEffect(() => {
+		const socialsNames = [
+			'facebook',
+			'instagram',
+			'twitter',
+			'twitch',
+			'youtube',
+		]
+		socialsNames.forEach((sName) => {
+			if (url.includes(sName)) {
+				setSocialName(sName)
+			}
+		})
+	}, [url])
+
+	useEffect(() => {
+		switch (socialName) {
+			case 'youtube':
+				setIcon(<FiYoutube />)
+				return
+			case 'facebook':
+				setIcon(<FaFacebookF />)
+				return
+			case 'instagram':
+				setIcon(<SiInstagram />)
+				return
+			case 'twitter':
+				setIcon(<FiTwitter />)
+				return
+			case 'twitch':
+				setIcon(<FiTwitch />)
+				return
+			default:
+				setIcon(<FaLink />)
+		}
+	}, [socialName])
+
+	return (
+		<Link href={url} target={'_blank'}>
+			<AspectRatio ratio={1} w={8}>
+				{icon}
+			</AspectRatio>
+		</Link>
+	)
+}
